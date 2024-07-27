fa78012d02e2767c11281dbd36234b518c1c1038
BlakeBoeckmann1 <105668230+BlakeBoeckmann1@users.noreply.github.com>
2024-07-27 14:42:12 -0500

    Update RealEstate.sol
    updated

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,28 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/tokens/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/tokens/ERC721/extentions/ERC721URIStorage.sol";
 
-}
+contract RealEstate is ERC721URIStorage {
+     using Counters for Counters.Counter;
+     Counters.Counters private_tokenIds;
+
+constructor() ERC721("MOBO GOBBLER", "MOBO") {}
+
+function awardItem(address player, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _mint(player, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
+    }
+    function totalSupply() public view returns (uint256){
+         return_tokenIds.current();
+    }
+
+{
